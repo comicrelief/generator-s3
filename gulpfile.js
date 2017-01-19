8fe8dafd8e7b2a181b9b67441dc95cf2c631110d
Peter Vanhee <peter.vanhee@gmail.com>
2017-01-19 11:10:04 +0000

    Adding Travis and generator test via Gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+var path = require('path');
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var nsp = require('gulp-nsp');
+var plumber = require('gulp-plumber');
+
+gulp.task('nsp', function (cb) {
+  nsp({package: path.resolve('package.json')}, cb);
+});
+
+gulp.task('test', [], function (cb) {
+  var mochaErr;
+
+  gulp.src('test/**/*.js')
+    .pipe(plumber())
+    .pipe(mocha({reporter: 'spec'}))
+    .on('error', function (err) {
+      mochaErr = err;
+    })
+    .on('end', function () {
+      cb(mochaErr);
+    });
+});
+
+gulp.task('prepublish', ['nsp']);
+gulp.task('default', ['test']);
